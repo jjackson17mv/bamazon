68bc3cd1791073604d4af5bfb504324871969fea
jax <jjackson17mv@gmail.com>
2019-05-26 16:50:50 -0700

    created connection with callback

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,22 @@
 var mysql = ("mysql");
-var inquirer = ('inquirer')
+var inquirer = ('inquirer');
+
+var connection = mysql.createconnection ({
+    host: 'localhost',
+    port: 7070,
+    user: 'root',
+    password: 'root',
+    database: 'bamazon'
+
+});
+
+
+//create connection to database
+
+
+connection.connect(function(err) {
+    if(err) throw err;
+    console.log("connected as id " + connection.threadId);
+
+  start();
+});
